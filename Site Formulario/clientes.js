82335e97b0688ec9d83b2356e8209d5e4a47bf17
Davi473 <68740118+Davi473@users.noreply.github.com>
2023-09-30 03:20:00 +0000

    alteração

--- Site Formulario/clientes.js
+++ Site Formulario/clientes.js
@@ -1,9 +1,11 @@
 
 const clientes = [
+    {cliente: "Cliente", email: "Email", telefone: "Telefone", stats: "Stats", acao: "Ação"}, // A acao vai ser algo para o futuro
     {cliente: "Davi Marcos Dorn", email: "davi###@gmail.com", telefone: "47 995####", stats: "Novo"},
     {cliente: "Fulano", email: "fulano@gmail.com", telefone: "47 7876####", stats: "Ligar Mais Tarde"}
 ]
 
-const value = [
-    "Cliente", "Email", "Telefone", "Stats"
+
+const valoresDoStats = [
+    "Novo", "Não Ligar Mais", "Ligar Mais Tarde"
 ]
