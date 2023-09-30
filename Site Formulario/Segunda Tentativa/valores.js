7fc0e02c7a430c57d4cd119096a106d214ee5568
Davi473 <68740118+Davi473@users.noreply.github.com>
2023-09-30 21:25:20 +0000

    alteração

---
+++ Site Formulario/Segunda Tentativa/valores.js
@@ -0,0 +1,14 @@
+
+
+const clientesAdicionados = [
+    {cliente: "Davi Marcos Dorn", email: "davi###@gmail.com", telefone: "47 995####", stats: "Novo"},
+    {cliente: "Fulano", email: "fulano@gmail.com", telefone: "47 7876####", stats: "Ligar Mais Tarde"}
+]
+
+const valoresDeStats = [
+    "Novo", "Não Ligar Mais", "Ligar Mais Tarde"
+]
+
+const valoresDaTabela = [
+    "Nome", "Email", "Telefone", "Stats", "Ação"
+]
