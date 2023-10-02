5f8947f6d8979b4173e28ded2ecb3ff1485f2df2
Davi473 <68740118+Davi473@users.noreply.github.com>
2023-10-02 00:31:01 -0300

    Add files via upload

---
+++ Site Formulario/Terceira Tentativa/clientes.js
@@ -0,0 +1,14 @@
+const clientesAdicionados = [
+    {cliente: "Cliente", email: "Email", telefone: "Telefone", stats: "Stats", acao: "Ação"},
+    {cliente: "Davi Marcos Dorn", email: "davi###@gmail.com", telefone: "47 995####", stats: "Novo", acao: "Excluir"},
+    {cliente: "Fulano", email: "fulano@gmail.com", telefone: "47 7876####", stats: "Ligar Mais Tarde", acao: "Excluir"}
+    //{cliente: "Paulo", email:}
+]
+
+const valoresDeStats = [
+    "Novo", "Não Ligar Mais", "Ligar Mais Tarde"
+]
+
+const valoresDaTabela = [
+    "nome", "email", "telefone", "stats", "acao"
+]
